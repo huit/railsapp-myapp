a67b7599d6f8ee5b6cf1226fceae6a5bbf613590
Steve Huff <shuff@vecna.org>
2013-10-31 13:20:07 -0400

    more Bundler shenanigans

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require "rvm/capistrano"
 require "bundler/capistrano"
+require "bundler"
 
 set :rvm_ruby_string, 'ruby-2.0.0-p247'   # use the same ruby as used locally for deployment
 set :rvm_autolibs_flag, "read-only"       # more info: rvm help autolibs
@@ -13,9 +14,13 @@ set :scm, :git # You can set :scm explicitly or Capistrano will make an intellig
 
 set :deploy_to, "/srv/www/rails/railsapp"
 
-role :web, "ec2-107-20-242-110.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
-role :app, "ec2-107-20-242-110.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
-role :db,  "ec2-107-20-242-110.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
+set :bundle_flags, "--deployment --binstubs"
+
+set :ssh_options, {:forward_agent => true, keys: ['~/.vagrant.d/insecure_private_key']}
+
+role :web, "10.0.2.15"                          # Your HTTP server, Apache/etc
+role :app, "10.0.2.15"                          # This may be the same as your `Web` server
+role :db,  "10.0.2.15", :primary => true # This is where Rails migrations will run
 
 before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 before 'bundle:install' do try_sudo('yum -y install sqlite-devel') end
